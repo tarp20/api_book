7c82521c1a13bc3f86646726037b069a5a9b04a9
Taras Piont <taras.piont@gmail.com>
2021-06-29 17:18:20 +0200

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import os
-from .local_settings import SECRET_KEY
+
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
