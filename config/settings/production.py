cddb0dadfe47b6a66b5221fd687437f5499e8dca
Taras Piont <taras.piont@gmail.com>
2021-06-29 17:23:58 +0200

    static

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,14 +10,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -42,6 +34,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +127,6 @@ REST_FRAMEWORK = {
     'SEARCH_PARAM': 'author',
     'ORDERING_PARAM': 'sort',
 }
+
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
